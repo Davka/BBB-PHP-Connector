9050a624f58b6fa2ed37f5e5c71787b10b217fda
David Siegfried <ds.siegfried@gmail.com>
2020-03-30 16:35:00 +0200

    refine

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Variablen
-php bin/console bbb:get-meetings --apiUrl https://webmeetings.uni-vechta.de/bigbluebutton/api --apiSecret csRpCQ2PHJ89XcjwJTNVEYqywztVTNjhh1zX5AtrE
-php bin/console bbb:get-meetings --apiUrl https://webmeetings.uni-vechta.de/bigbluebutton/api --apiSecret csRpCQ2PHJ89XcjwJTNVEYqywztVTNjhh1zX5AtrE
-php bin/console bbb:get-meetings --apiUrl https://webmeetings.uni-vechta.de/bigbluebutton/api --apiSecret csRpCQ2PHJ89XcjwJTNVEYqywztVTNjhh1zX5AtrE
-php bin/console bbb:get-meetings --apiUrl https://webmeetings.uni-vechta.de/bigbluebutton/api --apiSecret csRpCQ2PHJ89XcjwJTNVEYqywztVTNjhh1zX5AtrE
